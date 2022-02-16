e98ce36b002e6c2d98d91d380457b77f9a86c7ea
deddywolley <deddywolley@gmail.com>
2022-02-16 17:18:51 +0800

    reset app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import http from 'http'
+import cors from 'cors'
 const app = express()
+app.use(cors)
 const port = process.env.PORT || 3000
+const server = http.createServer(app, {})
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server listening on port ${port}`)
 })
